c68b180b1070ae4172d34ab3dadb1a3b607e302e
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-10 03:36:06 +0000

    deleted comment

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -20,7 +20,6 @@ export default class Header extends React.Component {
       <header className="row header">
         <div className="col my-auto px-0">
           <img onClick={this.toMain} className="logo" src="/images/iwearLogo.png" alt="iWearLogo"/>
-          {/* <h5 onClick={this.toMain} className="logo">John&apos;s Wicked Sale</h5> */}
         </div>
         <div className="col my-auto offset-md-4 px-0">
           <i className="fas fa-shopping-cart fa-lg float-right goToCart shopCart" onClick={this.viewCart}></i>
