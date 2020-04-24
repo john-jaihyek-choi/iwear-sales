520a906ca201d33f8970bf1088c36f31b8a56789
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-24 06:09:14 +0000

    header nav setting completed

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -49,20 +49,11 @@ export default class Header extends React.Component {
 
               <div className="collapse navbar-collapse" id="ftco-nav">
                 <ul className="navbar-nav ml-auto">
-                  <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
-                    <div className="dropdown-menu" aria-labelledby="dropdown04">
-                      <a className="dropdown-item" href="shop.html">Shop</a>
-                      <a className="dropdown-item" href="product-single.html">Single Product</a>
-                      <a className="dropdown-item" href="cart.html">Cart</a>
-                      <a className="dropdown-item" href="checkout.html">Checkout</a>
-                    </div>
-                  </li>
-                  <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
-                  <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
-                  <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
-                  <li className="nav-item cta cta-colored"><a href="cart.html" className="nav-link"><span className="icon-shopping_cart"></span>[0]</a></li>
+                  <li className="nav-item nav-link active">Home</li>
+                  <li className="nav-item nav-link">Shop</li>
+                  <li className="nav-item nav-link">About</li>
+                  <li className="nav-item nav-link">Contact</li>
+                  <li className="nav-item nav-link cta cta-colored"><i className='bx bxs-cart bx-sm'></i>[0]</li>
 
                 </ul>
               </div>
