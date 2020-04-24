d4c097ebdf4e388e6e2a37914c21f95de5d3fc9d
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-24 06:30:37 +0000

    header cleanup

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -40,11 +40,11 @@ export default class Header extends React.Component {
               </div>
             </div>
           </div>
-          <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
               <a className="navbar-brand" href="index.html">iWear</a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="oi oi-menu"></span> Menu
+              <button className="navbar-toggler" type="button" data-toggle="collapse">
+                <span></span> Menu
               </button>
 
               <div className="collapse navbar-collapse" id="ftco-nav">
@@ -53,7 +53,7 @@ export default class Header extends React.Component {
                   <li className="nav-item nav-link">Shop</li>
                   <li className="nav-item nav-link">About</li>
                   <li className="nav-item nav-link">Contact</li>
-                  <li className="nav-item nav-link cta cta-colored"><i className='bx bxs-cart bx-sm'></i>[0]</li>
+                  <li className="nav-item nav-link"><i className='bx bxs-cart bx-sm'></i>[0]</li>
 
                 </ul>
               </div>
