17bf51589764257295c537b2203382a48a041218
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-25 11:35:42 +0000

    basic textbox for landing carousel set

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -7,19 +7,26 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <OwlCarousel items={3} className="owl-theme" loop nav margin={8}>
-        <div className="item"><h4>1</h4></div>
-        <div className="item"><h4>2</h4></div>
-        <div className="item"><h4>3</h4></div>
-        <div className="item"><h4>4</h4></div>
-        <div className="item"><h4>5</h4></div>
-        <div className="item"><h4>6</h4></div>
-        <div className="item"><h4>7</h4></div>
-        <div className="item"><h4>8</h4></div>
-        <div className="item"><h4>9</h4></div>
-        <div className="item"><h4>10</h4></div>
-        <div className="item"><h4>11</h4></div>
-        <div className="item"><h4>12</h4></div>
+      <OwlCarousel items={1} className="owl-theme" autoplay margin={8} dots={false}>
+        <div className="">
+          <div className="container-fluid p-0">
+            <div className="row d-md-flex no-gutters align-items-center justify-content-end" data-scrollax-parent="true">
+              <div className="" style={{ backgroundImage: 'url(/assets/images/shake-weight.jpg)' }}></div> {/* where the image picture goes */}
+              <div className="d-flex align-items-center" data-scrollax=" properties: { translateY: '70%' }"> {/*  where the text boxes go */}
+                <div>
+                  <span className="subheading">Shop Men</span>
+                  <div>
+                    <h3>Best eCommerce Online Shop</h3> {/* Vertical Text */}
+                    <h1 className="mb-4 mt-3">A Thouroughly <span>Modern</span> Woman</h1>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
+
+                    <p><a href="#" className="btn btn-primary px-5 py-3 mt-3">Shop Now</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </OwlCarousel>
     );
   }
