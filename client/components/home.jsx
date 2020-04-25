2f5522eba0f969f8e48e4fa9f47198febf2488b1
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-25 07:35:20 +0000

    owl-carousel implemented successfully with installing npm loaders

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 export default class Home extends React.Component {
 
