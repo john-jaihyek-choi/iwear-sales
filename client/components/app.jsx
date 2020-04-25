39c12ebefa33ac4f397465dfca164c7d113c3678
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-25 00:19:04 +0000

    homepage structure setup

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import Home from './home';
 // import ProductList from './productList';
 // import ProductDetails from './product-details';
 // import CartSummary from './cartSummary';
@@ -11,7 +12,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'catalog',
+        name: 'home',
         params: {}
       },
       cart: []
@@ -93,10 +94,15 @@ export default class App extends React.Component {
   }
 
   render() {
+    let content = null;
+    if (this.state.view.name === 'home') {
+      content = <Home />;
+    }
+
     return (
       <>
         <Header />
-
+        {content}
         <Footer />
       </>
     );
