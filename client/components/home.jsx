124444fd949a4324862b5a49edc32d1ed3ab8d92
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-28 05:01:54 +0000

    minor changes on the home.jsx and checkpoint

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -13,12 +13,15 @@ export default class Home extends React.Component {
             <div className="row d-md-flex no-gutters align-items-center justify-content-end" data-scrollax-parent="true">
               <div className="" style={{ backgroundImage: 'url(/assets/images/shake-weight.jpg)' }}></div> {/* where the image picture goes */}
               <div className="d-flex align-items-center" data-scrollax=" properties: { translateY: '70%' }"> {/*  where the text boxes go */}
-                <div>
+                <div className="textbox position-relative">
                   <span className="subheading">Shop Men</span>
-                  <div>
-                    <h3>Best eCommerce Online Shop</h3> {/* Vertical Text */}
-                    <h1 className="mb-4 mt-3">A Thouroughly <span>Modern</span> Woman</h1>
-                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
+                  <div className="mainContent position-relative">
+                    <div className="col-12 col-sm-6 rotate-sm-l-90">
+                      <h3 className="h6">Best eCommerce Online Shop</h3>
+                    </div>
+
+                    <h1 className="mainHeading mb-4 mt-3">A Thouroughly <span>Modern</span> Woman</h1>
+                    <p className="subtitle">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
 
                     <p><a href="#" className="btn btn-primary px-5 py-3 mt-3">Shop Now</a></p>
                   </div>
