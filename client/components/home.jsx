25d7af0b00a1447f91844de904d9437cc7029164
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-29 04:55:49 +0000

    working fresh on gridlayout of owl carousel

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -8,18 +8,15 @@ export default class Home extends React.Component {
   render() {
     return (
       <OwlCarousel items={1} className="owl-theme" autoplay margin={8} dots={false}>
-        <div className="container d-flex">
-          <div className='row w-50'>
-            <div className='col-6'>
-              <div className='textIntro'>
-                test
-              </div>
-            </div>
-          </div>
-          <div className='row w-50'>
-            <div className='col-6'>
-              <div className="imgIntro">
-                Image
+        <div className="container d-flex container-fluid">
+          <div className='row p-4'>
+            <div className='col p-4 p-md-1 p-sm-0'>
+              <div className='textIntro d-flex p-4'>
+                <div className="col-12 horizontal">
+                  <h1 className="mb-4 mt-3">A Thouroughly <span>Modern</span> Woman</h1>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
+                  <p><a href="#" className="btn btn-primary px-5 py-3 mt-3">Shop Now</a></p>
+                </div>
               </div>
             </div>
           </div>
