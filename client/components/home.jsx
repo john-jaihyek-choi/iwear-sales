af45298f8cdb2b53acaf8b4f088050b215a4d28f
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-29 10:48:18 +0000

    set additional media query rules for textbox position

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -10,11 +10,11 @@ export default class Home extends React.Component {
       <OwlCarousel items={1} className="owl-theme" autoplay margin={8} dots={false}>
         <div className="opticalCarousel container-fluid">
           <div className='carouselTextBox col-lg-6 col-md-6 position-relative d-flex justify-content-center'>
-            <div className='carouselContent position-absolute mx-auto align-items-center'>
+            <div className='carouselContent position-absolute mx-auto align-items-center p-2'>
               <h5>OPTICAL GLASSES</h5>
               <h3 className='font-weight-bold'>Explore our selection of fashion opitcal glasses to find the perfect frame that suits you!</h3>
               <div className='d-flex pt-4 text-center'>
-                <button className='col btn mr-4'>SHOP MEN</button>
+                <button className='col btn mr-2'>SHOP MEN</button>
                 <button className='col btn'>SHOP WOMEN</button>
               </div>
             </div>
@@ -22,11 +22,11 @@ export default class Home extends React.Component {
         </div>
         <div className="sunglassCarousel container-fluid row justify-content-end m-0 p-0">
           <div className='carouselTextBox col-lg-6 col-md-6 position-relative d-flex justify-content-center'>
-            <div className='carouselContent position-absolute mx-auto align-items-center'>
+            <div className='carouselContent position-absolute mx-auto align-items-center p-2'>
               <h5>SUNGLASSES</h5>
               <h3 className='font-weight-bold'>Protect your eyes from the sun with our latest sunglass collection!</h3>
               <div className='d-flex pt-4 text-center'>
-                <button className='col btn mr-4'>SHOP MEN</button>
+                <button className='col btn mr-2'>SHOP MEN</button>
                 <button className='col btn'>SHOP WOMEN</button>
               </div>
             </div>
