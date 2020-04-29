5beee8e16eccb34ee8c18dc79686683a73b804d8
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-29 09:38:04 +0000

    basic structure and position set for mobile and web for landing carousel

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -9,15 +9,25 @@ export default class Home extends React.Component {
     return (
       <OwlCarousel items={1} className="owl-theme" autoplay margin={8} dots={false}>
         <div className="opticalCarousel container-fluid">
-          <div className='carouselTextBox col-lg-6 col-md-6'>
-              test
+          <div className='carouselTextBox col-lg-6 col-md-6 position-relative d-flex justify-content-center'>
+            <div className='carouselContent position-absolute mx-auto align-items-center'>
+              <h3>Optical Glasses</h3>
+              <h5>Explor our selection of fashion opitcal glasses to find the perfect frame that suits you best!</h5>
+              <button className='btn btn-primary'>Shop Men</button>
+              <button className='btn btn-primary'>Shop Women</button>
+            </div>
           </div>
         </div>
-        {/* <div className="sunglassCarousel container-fluid">
-          <div className='row'>
-
+        <div className="sunglassCarousel container-fluid row justify-content-end m-0 p-0">
+          <div className='carouselTextBox col-lg-6 col-md-6 position-relative d-flex justify-content-center'>
+            <div className='carouselContent position-absolute mx-auto align-items-center'>
+              <h3>Optical Glasses</h3>
+              <h5>Explor our selection of fashion opitcal glasses to find the perfect frame that suits you best!</h5>
+              <button className='btn btn-primary'>Shop Men</button>
+              <button className='btn btn-primary'>Shop Women</button>
+            </div>
           </div>
-        </div> */}
+        </div>
       </OwlCarousel>
     );
   }
