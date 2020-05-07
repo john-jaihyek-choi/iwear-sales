ffc29ca44b30106bd58743d9dcf60bda9d5302c8
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-07 09:33:09 +0000

    nav stylings changed

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -101,8 +101,10 @@ export default class App extends React.Component {
     return (
       <BrowserRouter>
         <Header toggleView={this.toggleView}/>
+
         <Route exact path='/' component={Home} />
         <Route path='/shop' component={Shop} />
+
         <Footer />
       </BrowserRouter>
     );
