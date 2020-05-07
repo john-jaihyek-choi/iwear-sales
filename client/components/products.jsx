cc2c02cccb21423b8de178f68a77fb8362363d0f
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-07 12:03:43 +0000

    hover effect applied on product images

--- client/components/products.jsx
+++ client/components/products.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 
 export default class Products extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imgHidden: false
-    };
-    this.hideImg = this.hideImg.bind(this);
-  }
-
-  hideImg(event) {
-    this.setState({
-      imgHidden: true
-    });
-  }
 
   render() {
     const test = this.props.products.map(product => {
@@ -26,8 +13,8 @@ export default class Products extends React.Component {
       const productInfo = <li key={name} className='col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-4 mt-4'>
         <div className="productInfo text-center">
           <div className="productImage">
-            <img src={image2} className='front imgFade' alt={image2}/>
-            <img src={image1} className={`side imgShow ${this.state.imgHidden ? 'hidden' : ''}`} alt={image1} onMouseEnter={this.hideImg}/>
+            <img src={image2} className='side' alt={image2} />
+            <img src={image1} className='front' alt={image1} />
           </div>
           <div className="productDetails">
             <span>{name}</span><br/>
