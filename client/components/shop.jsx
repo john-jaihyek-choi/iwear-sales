6584cc8d31bc6d717950fba91fee041e1327ff09
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-13 11:30:14 +0000

    shop component useEffect async issue resolved with keeping track of states

--- client/components/shop.jsx
+++ client/components/shop.jsx
@@ -1,37 +1,41 @@
-import React, { useState, useEffect, lazy, Suspense } from 'react';
-const Products = lazy(() => import('./products'));
+import React, { useState, useEffect } from 'react';
+import Products from './products';
 
 const Shop = props => {
   const [products, setProducts] = useState([]);
   const [swatches, setSwatches] = useState([]);
+  const [productFetchStatus, setProductFetchStatus] = useState(false);
+  const [swatchFetchStatus, setSwatchFetchStatus] = useState(false);
 
-  const getProducts = () => {
-    fetch('/api/products')
+  const getProducts = async () => {
+    await fetch('/api/products')
       .then(promise => promise.json())
       .then(products => {
         setProducts(products);
+        setProductFetchStatus(true);
       });
   };
 
-  const getSwatches = () => {
-    fetch('/api/swatches')
+  const getSwatches = async () => {
+    await fetch('/api/swatches')
       .then(promise => promise.json())
       .then(swatches => {
         setSwatches(swatches);
+        setSwatchFetchStatus(true);
       });
   };
 
   useEffect(() => {
-    getProducts();
-    getSwatches();
+    if (!productFetchStatus && !swatchFetchStatus) {
+      getProducts();
+      getSwatches();
+    }
   }, []);
 
   return (
     <div className="container">
       <ul className='list-unstyled d-flex flex-wrap'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Products products={products} swatches={swatches}/>
-        </Suspense>
+        {(productFetchStatus && swatchFetchStatus) ? <Products products={products} swatches={swatches}/> : <div>Loading...</div>}
       </ul>
     </div>
   );
