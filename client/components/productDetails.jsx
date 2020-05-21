7a52ee3e0a27ccd6522346b624e8219a8f721036
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-21 10:34:16 +0000

    client side fetch call created

--- client/components/productDetails.jsx
+++ client/components/productDetails.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ProductDetails = props => {
+  const [details, setDetails] = useState(props.location.productProps || null);
+
+  const getProductDetail = () => {
+    fetch('/api/getDetails')
+      .then(promise => promise.json())
+      .then(details => {
+        setDetails(details);
+      });
+  };
+
+  useEffect(() => {
+    if (!details) {
+      getProductDetail();
+    }
+  });
+
   return (
     <div className='container'>
       <div className='row'>
