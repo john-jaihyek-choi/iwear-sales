742e53681e37a24d169ff5ca7c5d3261d288b139
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-21 10:24:38 +0000

    details top nav path created

--- client/components/productDetails.jsx
+++ client/components/productDetails.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 const ProductDetails = props => {
   return (
-    <div>test</div>
+    <div className='container'>
+      <div className='row'>
+        <ul className='list-unstyled d-flex'>
+          <li className='mx-1'>Home</li>
+          <li>&gt;</li>
+          <li className='mx-1'>Shop</li>
+          <li>&gt;</li>
+          <li className='mx-1'>{props.match.params.productName.toUpperCase()}</li>
+        </ul>
+      </div>
+      <div>
+
+      </div>
+    </div>
   );
 };
 
