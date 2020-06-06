e01b6504893e32c536070df4af111d99e1745db2
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-06-06 00:12:13 +0000

    restored to original state

--- client/components/shop.jsx
+++ client/components/shop.jsx
@@ -4,15 +4,14 @@ import Products from './products';
 const Shop = props => {
   const [products, setProducts] = useState([]);
   const [swatches, setSwatches] = useState([]);
-  const [productFetchStatus, setProductFetchStatus] = useState(false);
-  const [swatchesFetchStatus, setSwatchesFetchStatus] = useState(false);
+  const [fetchStatus, setFetchStatus] = useState(false);
 
   const getProducts = () => {
     fetch('/api/products')
       .then(promise => promise.json())
       .then(products => {
         setProducts(products);
-        setProductFetchStatus(true);
+        setFetchStatus(true);
       });
   };
 
@@ -21,16 +20,13 @@ const Shop = props => {
       .then(promise => promise.json())
       .then(swatches => {
         setSwatches(swatches);
-        setSwatchesFetchStatus(true);
       });
   };
 
   useEffect(() => {
-    if (!productFetchStatus) {
-      getProducts();
-    }
-    if (!swatchesFetchStatus) {
+    if (!fetchStatus) {
       getSwatches();
+      getProducts();
     }
   });
 
@@ -38,7 +34,7 @@ const Shop = props => {
     <>
       <div className="container">
         <ul className='list-unstyled d-flex flex-wrap'>
-          {(productFetchStatus && swatchesFetchStatus) ? <Products products={products} swatches={swatches}/> : <div>Loading...</div>}
+          {(fetchStatus) ? <Products products={products} swatches={swatches}/> : <div>Loading...</div>}
         </ul>
       </div>
     </>
